ff8e34d45d958a38b3ece1f3cb7595a88831ec63
田中里歩 <tanaka.riho24@gmail.com>
2023-12-20 22:32:33 +0900

    🩹Fix:不要な記述の削除

--- front/app/components/top/Title.tsx
+++ front/app/components/top/Title.tsx
@@ -3,21 +3,17 @@ import React from 'react';
 const Title = () => {
 	return (
 		<div className="flex justify-center items-center">
-				<div className="my-20 py-12 px-4 sm:px-6 lg:px-8">
-					<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
-						<div className="flex items-center justify-center space-x-3">
-							<h1 className="ml-5 text-6xl font-bold sm:text-7xl tracking-wide">
+					<div className="relative mx-auto container">
+							<h1 className="text-6xl text-center mt-5">
 								ヨガ図鑑
 							</h1>
-						</div>
-						<p className="mt-6 font-semibold text-2xl leading-7">
+						<p className="mt-6  text-2xl  text-center">
 							ヨガのポーズの図鑑を完成させるアプリです
 						</p>
-						<p className="mt-1 font-semibold text-2xl leading-7">
+						<p className="mt-1  text-2xl text-center">
 							様々なヨガに関するコンテンツを体験して、楽しくヨガのポーズを集めて覚えましょう！
 						</p>
 					</div>
-				</div>
 		</div>
 	);
 };
