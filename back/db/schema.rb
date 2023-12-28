00863fc18b050b1f5b8bcc4c8d7880b55c8ea766
田中里歩 <tanaka.riho24@gmail.com>
2023-12-28 20:44:31 +0900

    ✨Add:Answerモデルの作成

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_28_093323) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_28_114157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content", null: false
+    t.boolean "correct", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -38,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_093323) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "quizzes"
 end
