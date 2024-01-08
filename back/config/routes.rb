d5c4bd8abaef2eabd8a7b4fa23932ff8891c0184
田中里歩 <tanaka.riho24@gmail.com>
2024-01-08 11:07:21 +0900

    :zap:Add：Quiestionsのルーティング追加

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :quizzes
+      resources :questions
     end
   end
   post 'auth/:provider/callback', to: 'api/v1/users#create'
