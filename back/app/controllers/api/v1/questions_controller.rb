ea5dfeac5baee6a440d2c90cca593468a890a799
田中里歩 <tanaka.riho24@gmail.com>
2024-01-09 18:36:16 +0900

    :adhesive_bandage: Fix:Quizに紐づいたQuestionとAnswerを取得するように修正

--- back/app/controllers/api/v1/questions_controller.rb
+++ back/app/controllers/api/v1/questions_controller.rb
@@ -1,22 +1,4 @@
 class Api::V1::QuestionsController < ApplicationController
-  before_action :set_question, only: [:show]
-
   def index
-    @questions = Question.all
-    render json: @questions
-  end
-
-  def show
-    render json: @question
-  end
-
-  private
-
-  def set_question
-    @question = Question.find(params[:id])
-  end
-
-  def question_params
-    params.require(:question).permit(:title, :quiz_id)
   end
 end
--- back/app/controllers/api/v1/quizzes_controller.rb
+++ back/app/controllers/api/v1/quizzes_controller.rb
@@ -7,25 +7,13 @@ class Api::V1::QuizzesController < ApplicationController
   end
 
   def show
-    render json: @quiz
+    questions = @quiz.questions.includes(:answers)
+    render json: questions, include: :answers
   end
 
-  # def create
-  #  @quiz = Quiz.new(quiz_params)
-
-  #   if @quiz.save
-  #     render json: @quiz, status: :created 
-  #   else 
-  #     render json: @quiz.errors, status: :unprocessable_entity
-  #   end
-  # end
 
   private 
-
-  def quiz_params
-    params.require(:quiz).permit(:title, :level)
-  end
-
+  
   def set_quiz 
      @quiz = Quiz.find(params[:id])
   end
