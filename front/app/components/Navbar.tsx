64456c75180b66c6fd7201f8324641d484cba285
田中里歩 <tanaka.riho24@gmail.com>
2024-01-09 18:28:56 +0900

    :lipstick: Add:ハンバーガーメニューの実装、リンクの修正、フッターの修正

--- front/app/components/Navbar.tsx
+++ front/app/components/Navbar.tsx
@@ -1,28 +1,70 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import { useState } from 'react';
 
 const Navbar = () => {
-    return (
-    <div className = 'h-20 w-full border-b-2 flex items-center justify-between p-2'>
-      <ul className = 'flex'>
-        <li className = 'p-2 cursor-pointer'>
-          <Link href='/'>Home</Link>
-        </li>
-        <li className = 'p-2 cursor-pointer'>
-          <Link href='/profile'>ヨガクイズテスト</Link>
-        </li>
-        <li className = 'p-2 cursor-pointer'>
-          <Link href='/question'>ヨガクイズ</Link>
-        </li>
-      </ul>
-      <ul className = 'flex'>
-      <li className = 'p-2 cursor-pointer'>
-          <Link href='/'>Login</Link>
-      </li>
-      <li className = 'p-2 cursor-pointer'>
-          <Link href='/'>Sign up</Link>
-      </li>
-      </ul>
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenuOpen = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  return (
+    <div className="App">
+      <div className="container mx-auto px-3">
+        <header className="flex justify-between py-3">
+          <h1>ヨガ図鑑</h1>
+
+          {/* humbergerbutton */}
+          <button
+            onClick={handleMenuOpen}
+            type="button"
+            className="z-10 space-y-2"
+          >
+            <div
+              className={
+                openMenu
+                  ? "w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 transition duration-500 ease-in-out"
+                  : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
+              }
+            />
+            <div
+              className={
+                openMenu
+                  ? "opacity-0 transition duration-500 ease-in-out"
+                  : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
+              }
+            />
+            <div
+              className={
+                openMenu
+                  ? "w-8 h-0.5 bg-gray-600 -rotate-45 transition duration-500 ease-in-out"
+                  : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
+              }
+            />
+          </button>
+          <nav
+            className={
+              openMenu
+                ? "text-center fixed bg-slate-50 right-0 top-0 w-8/12 h-screen flex flex-col justify-start pt-8 px-3 ease-linear duration-300"
+                : "fixed right-[-100%] ease-linear duration-300"
+            }
+          >
+            <ul className="mt-6">
+              <li className = 'p-2 cursor-pointer'>
+                <Link href='/'>Top</Link>
+              </li>
+              <li className = 'p-2 cursor-pointer'>
+                <Link href='/quiz'>ヨガ検定</Link>
+              </li>
+              <li className = 'p-2 cursor-pointer'>
+                <Link href='/'>未定</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
     </div>
   )
 }
