74a4a9a667adb4c9f02294e97e352086bbb79710
田中里歩 <tanaka.riho24@gmail.com>
2024-01-09 18:40:39 +0900

    :construction: WIP:ヨガ検定：答えと問題を取得してそれぞれで表示するところまで

---
+++ front/app/question/[slug]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+import useSWR from "swr";
+import { fetcher } from "@/app/_common/utils";
+
+import { Card } from "@mui/material";
+
+import { useParams } from 'next/navigation'
+ 
+export default function ExampleClientComponent() {
+  const { slug } = useParams<{ slug: string }>()
+  console.log(slug)
+
+  const url =`http://localhost:3000/api/v1/quizzes/${slug}`;
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>An error has occurred.</div>
+  if (!data) return <div>Loading...</div>
+
+ 
+  console.log(data)
+  return (
+  <>
+      <h1>これはヨガ検定の問題です。</h1>
+      <div className="mx-auto w-8/12 mt-10">
+        <div className="grid grid-cols-2 gap-4">
+          {data.map((question: any) => (
+            <Card key={question.id} className='p-5 border border-gray-200 rounded-lg'>
+              <div>問題: {question.title}</div>
+                {question.answers.map((answer :any) => (
+              <ul>
+                  <li key={answer.id}>・{answer.content}</li>
+              </ul>
+                ))}
+            </Card>
+          ))}
+        </div>
+      </div>
+  </>
+)}
--- front/app/quiz/page.tsx
+++ front/app/quiz/page.tsx
@@ -1,26 +1,50 @@
-// 'use client'
-// import type { NextPage } from 'next'
-// import useSWR from 'swr'
-// import { fetcher } from '../_common/utils'
+'use client'
+import React from "react";
+import useSWR from 'swr';
+import { fetcher } from "../_common/utils";
+import Link from "next/link";
 
-// const Index: NextPage = () => {
-//   const url = 'http://localhost:3000/api/v1/quizzes'
-//   const { data, error } = useSWR(url, fetcher)
 
-//   if (error) return <div>An error has occurred.</div>
-//   if (!data) return <div>Loading...</div>
+interface Answer {
+  id: number;
+  content: string;
 
-//   return (
-//     <>
-//       <div>Rails疎通確認</div>
-//       {data.map((quiz: any) => (
-//         <div key={quiz.id}>
-//           <div>Title: {quiz.title}</div>
-//           <div>Level: {quiz.level}</div>
-//         </div>
-//       ))}
-//     </>
-//   )
-// }
+}
 
-// export default Index
+interface quiz {
+  id: number;
+  title: string;
+  answers: Answer[];
+
+}
+
+interface Quiz {
+  id: number;
+  title: string;
+  level: string;
+  quizs: quiz[];
+}
+
+const QuizPage: React.FC = () => {
+  const url = 'http://localhost:3000/api/v1/quizzes'
+  const { data, error } = useSWR(url, fetcher);
+
+  if (error) return <div>An error has occurred.</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <>
+        {data.map((quiz: any) => (
+          <div className="container">
+            <Link href={`/question/${quiz.id}`}>
+              <div key={quiz.id}>
+                <h3>{quiz.title}</h3>
+              </div>
+            </Link>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default QuizPage;
