008b10f9331abc31a3002a56f89f49e55078edb6
田中里歩 <tanaka.riho24@gmail.com>
2024-01-12 18:39:22 +0900

    :zap:Fix:アソシエーションの修正

--- back/app/models/question.rb
+++ back/app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   has_many :answers, dependent: :destroy
+  has_many :user_answers, dependent: :destroy
 end
--- back/app/models/quiz.rb
+++ back/app/models/quiz.rb
@@ -1,3 +1,4 @@
 class Quiz < ApplicationRecord
   has_many :questions, dependent: :destroy
+  has_many :quiz_result
 end
