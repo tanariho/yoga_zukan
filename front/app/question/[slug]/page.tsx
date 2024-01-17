d9d5fbd7bcb2ba6945f7f9b033c5e9800af80c50
田中里歩 <tanaka.riho24@gmail.com>
2024-01-17 21:00:47 +0900

    :construction:WIP:検定に合格するとポーズが付与される|画像の大きさ修正|ポーズを全て取得した場合の処理

--- front/app/question/[slug]/page.tsx
+++ front/app/question/[slug]/page.tsx
@@ -4,6 +4,8 @@ import useSWR from "swr";
 import axios from "axios";
 import { fetcher } from "@/app/_common/utils";
 import { useParams } from "next/navigation";
+import Image from "next/image";
+
 import {
   Card,
   Radio,
@@ -15,18 +17,17 @@ import { useSession } from "next-auth/react";
 import { nextAuthOptions } from "@/lib/next-auth/options";
 import fetchUserId from "@/app/components/fetcher/user/FetchUser";
 
-
-
 export default function ExampleClientComponent() {
   const { data: session, status } = useSession();
-  const params = useParams()
-  const slug = params.slug
+  const params = useParams();
+  const slug = params.slug;
   const url = `http://localhost:3000/api/v1/quizzes/${slug}`;
   const { data, error } = useSWR<Question[]>(url, fetcher);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState<Answers>({});
   const [submitted, setSubmitted] = useState(false);
   const [userId, setUserId] = useState<number | null>(null);
+  const [yogaPose, setYogaPose] = useState<YogaPose | null>(null);
 
   useEffect(() => {
     const fetchCurrentUserId = async () => {
@@ -36,7 +37,7 @@ export default function ExampleClientComponent() {
           console.log("取得されたユーザーID:", UserId);
           setUserId(UserId);
         } catch (error) {
-          console.error('ユーザーIDの取得に失敗しました:', error);
+          console.error("ユーザーIDの取得に失敗しました:", error);
         }
       }
     };
@@ -47,7 +48,7 @@ export default function ExampleClientComponent() {
   }, [session]);
 
   const handleAnswerChange = (answerId: any) => {
-    const question = data?.[currentQuestionIndex]
+    const question = data?.[currentQuestionIndex];
     if (question) {
       setAnswers({ ...answers, [question.id]: answerId });
       if (currentQuestionIndex < data.length - 1) {
@@ -55,14 +56,19 @@ export default function ExampleClientComponent() {
       } else {
         setSubmitted(true);
       }
-      console.log('クエスチョンです',currentQuestionIndex)
+      console.log("クエスチョンです", currentQuestionIndex);
     }
   };
 
   const calculateResult = () => {
     let incorrectCount = 0;
-    data?.forEach((question :Question) => {
-      if (!question.answers.some((answer :Answer) => answer.correct && answers[question.id] === answer.id)) {
+    data?.forEach((question: Question) => {
+      if (
+        !question.answers.some(
+          (answer: Answer) =>
+            answer.correct && answers[question.id] === answer.id
+        )
+      ) {
         incorrectCount++;
       }
     });
@@ -72,16 +78,33 @@ export default function ExampleClientComponent() {
   const submitResult = async () => {
     if (userId !== null && slug) {
       const passed = calculateResult();
-      console.log(passed)
+      console.log(passed);
       try {
-        await axios.post('http://localhost:3000/api/v1/quiz_results', {
+        const response = await axios.post(
+          "http://localhost:3000/api/v1/quiz_results",
+          {
             user_id: userId,
             quiz_id: slug,
-            passed: passed
-        });
-        // ここで応答を処理
+            passed: passed,
+          }
+        );
+
+        const { passed_true, assigned_pose } = response.data;
+        console.log(response.data);
+        console.log(assigned_pose);
+
+        if (passed_true) {
+          if (assigned_pose) {
+            // 割り当てられたヨガポーズを状態に保存し、画面に表示する
+            setYogaPose(assigned_pose);
+          } else {
+            // ユーザーがすでにすべてのポーズを制覇している場合
+            alert("すでにポーズを制覇しています！");
+            // または、状態やUIコンポーネントを使用してメッセージを表示
+          }
+        }
       } catch (error) {
-        console.error('Quiz result submission failed', error);
+        console.error("Quiz result submission failed", error);
       }
     }
   };
@@ -101,12 +124,12 @@ export default function ExampleClientComponent() {
 
   return (
     <>
-      <h1>これはヨガ検定の問題です。</h1>
       <div className="mx-auto w-8/12 mt-10">
-        <Card className="p-5 border border-gray-200 rounded-lg">
-          <div>問題: {currentQuestion.title}</div>
+        <Card className=" mx-auto justify-items-center p-5  bg-stone-200 border border-gray-200 bg-opacity-75 rounded-3xl shadow">
+          <div className="text-center">{currentQuestion.title}</div>
           <RadioGroup
             onChange={(e) => handleAnswerChange(parseInt(e.target.value, 10))}
+            row
           >
             {currentQuestion.answers.map((answer: Answer) => (
               <FormControlLabel
@@ -122,7 +145,22 @@ export default function ExampleClientComponent() {
       </div>
       {submitted && (
         <div className="mx-auto justify-items-center">
-          {calculateResult() ? '合格です！' : '不合格です。'}
+          {calculateResult() ? "合格です！" : "不合格です。"}
+        </div>
+      )}
+      {yogaPose && (
+        <div>
+          <h3>{yogaPose.japanese_name}</h3>
+          {/* <img src={yogaPose.illustration} alt={yogaPose.japanese_name} /> */}
+          <Image
+            src={yogaPose.illustration}
+            alt={yogaPose.japanese_name}
+            width={200}
+            height={200}
+            style={{ width: "auto", height: "auto" }}
+          />
+          <p>{yogaPose.sanskrit_name}</p>
+          <p>{yogaPose.how_to_read}</p>
         </div>
       )}
     </>
