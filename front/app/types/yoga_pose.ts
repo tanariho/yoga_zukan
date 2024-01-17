9e0a98ffa060343790bc0c8329ee2417c13a2307
田中里歩 <tanaka.riho24@gmail.com>
2024-01-17 20:56:21 +0900

    :label:Add:YogaPoseの型定義

---
+++ front/app/types/yoga_pose.ts
@@ -0,0 +1,11 @@
+interface YogaPose {
+ id: string
+ title: string;
+ japanese_name: string;
+ sanskrit_name: string;
+ how_to_read: string;
+ number: number;
+ level: string;
+ explanation: string;
+ illustration: string;
+};
