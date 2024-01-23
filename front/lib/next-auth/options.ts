5a8b3fc92d2083fccc3b144395e84853dea36fa4
田中里歩 <tanaka.riho24@gmail.com>
2024-01-23 13:35:00 +0900

    🩹Fix:本番環境でログインできるように修正

--- front/lib/next-auth/options.ts
+++ front/lib/next-auth/options.ts
@@ -8,8 +8,8 @@ export const nextAuthOptions: NextAuthOptions = {
 	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
+			clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? '',
 		}),
 	],
 	callbacks: {
