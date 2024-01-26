b7fa592ed6b322099b06a08a9bbc275623c6ee1f
田中里歩 <tanaka.riho24@gmail.com>
2024-01-26 22:18:58 +0900

    :art:Update:ヨガポーズ付与画面・ロジック改良/ボタンのコンポーネント化

---
+++ front/app/components/yoga_timer/BackButton.tsx
@@ -0,0 +1,23 @@
+import { Button } from "primereact/button";
+import Link from "next/link";
+
+const YogaTimerBackBuntton = () => {
+  return (
+    <div className="flex justify-center mb-10">
+      <Link href="/yoga_zukan">
+        <Button
+          style={{
+            backgroundColor: "#e2a55e",
+            color: "white",
+            border: "none",
+          }}
+          className="p-button-warning"
+        >
+          ヨガ図鑑を見に行く
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default YogaTimerBackBuntton;
