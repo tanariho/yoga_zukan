9d869785d6a14ed0681656121f77376cdd701248
田中里歩 <tanaka.riho24@gmail.com>
2024-01-30 18:45:52 +0900

    :bug:Fix:min.css追加

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,12 +1,13 @@
-import "./globals.css";
+import { PrimeReactProvider } from "primereact/api";
 import type { Metadata } from "next";
 import { Inconsolata } from "next/font/google";
 import Navbar from "./components/Navbar";
 import NextAuthProvider from "./providers/NextAuth";
 import Footer from "./components/top/Footer";
-import { PrimeReactProvider } from "primereact/api";
+import "./globals.css";
 import "primeicons/primeicons.css";
-import "primereact/resources/themes/tailwind-light/theme.css"
+import "primereact/resources/themes/tailwind-light/theme.css";
+import "primereact/resources/primereact.min.css";
 import { ToastProvider } from "./components/context/ToastContext";
 
 const inter = Inconsolata({ subsets: ["latin"] });
