7624df8dc675ccfa319f451ef41bd62e746e7a88
田中里歩 <tanaka.riho24@gmail.com>
2024-01-30 16:33:25 +0900

    :lipstick: Update:CSSをtailwindのテーマに

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import NextAuthProvider from "./providers/NextAuth";
 import Footer from "./components/top/Footer";
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/themes/tailwind-light/theme.css"
 import "primeicons/primeicons.css";
 import { ToastProvider } from "./components/context/ToastContext";
 
