fc5653d2d4cda32af4564002c0ae8fd4e67a0821
田中里歩 <tanaka.riho24@gmail.com>
2024-01-30 18:33:55 +0900

    :bug:Fix:cssの読み込み順序変更 global.css Providerの削除6回目

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/primereact/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
